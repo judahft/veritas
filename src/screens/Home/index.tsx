68493871ef7f80718adb7dc93646ea2d5feb45c4
Jovan Frandika <jopantaimak@gmail.com>
2021-12-07 09:00:25 +0700

    chore(env): setup .env

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Config from 'react-native-config';
 
 import { Typography } from '@components';
 
 const Home = () => (
   <SafeAreaView>
+    <Typography>
+      {Config.APP_TITLE}
+    </Typography>
     <Typography>
       Test
     </Typography>
